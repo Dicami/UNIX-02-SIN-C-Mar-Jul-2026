026caa11131ac2d7d1471609c580486b3dc0f950
Dicami <divillagranpa@uide.edu.ec>
2026-03-20 15:59:19 +0000

    resultados

---
+++ gnup_script.sh
@@ -0,0 +1,152 @@
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (main) $ uname -a
+Linux codespaces-9db7a6 6.8.0-1044-azure #50~22.04.1-Ubuntu SMP Wed Dec  3 15:13:22 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (main) $ which gpg
+/usr/bin/gpg
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (main) $ gpg --version
+gpg (GnuPG) 2.4.4
+libgcrypt 1.10.3
+Copyright (C) 2024 g10 Code GmbH
+License GNU GPL-3.0-or-later <https://gnu.org/licenses/gpl.html>
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+
+Home: /home/codespace/.gnupg
+Supported algorithms:
+Pubkey: RSA, ELG, DSA, ECDH, ECDSA, EDDSA
+Cipher: IDEA, 3DES, CAST5, BLOWFISH, AES, AES192, AES256, TWOFISH,
+        CAMELLIA128, CAMELLIA192, CAMELLIA256
+Hash: SHA1, RIPEMD160, SHA256, SHA384, SHA512, SHA224
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (main) $ gpg --full-generate-key
+gpg (GnuPG) 2.4.4; Copyright (C) 2024 g10 Code GmbH
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+
+gpg: directory '/home/codespace/.gnupg' created
+gpg: keybox '/home/codespace/.gnupg/pubring.kbx' created
+Please select what kind of key you want:
+   (1) RSA and RSA
+   (2) DSA and Elgamal
+   (3) DSA (sign only)
+   (4) RSA (sign only)
+   (9) ECC (sign and encrypt) *default*
+  (10) ECC (sign only)
+  (14) Existing key from card
+Your selection? 1
+RSA keys may be between 1024 and 4096 bits long.
+What keysize do you want? (3072) 4096
+Requested keysize is 4096 bits
+Please specify how long the key should be valid.
+         0 = key does not expire
+      <n>  = key expires in n days
+      <n>w = key expires in n weeks
+      <n>m = key expires in n months
+      <n>y = key expires in n years
+Key is valid for? (0) 1y
+Key expires at Sat Mar 20 15:40:39 2027 UTC
+Is this correct? (y/N) y
+
+GnuPG needs to construct a user ID to identify your key.
+
+Real name: DIANA CAMILA VILLAGRAN PARRALES
+Email address: dianacamilux@gmail.com
+Comment: mi primera llave 
+You selected this USER-ID:
+    "DIANA CAMILA VILLAGRAN PARRALES (mi primera llave) <dianacamilux@gmail.com>"
+
+Change (N)ame, (C)omment, (E)mail or (O)kay/(Q)uit? O
+We need to generate a lot of random bytes. It is a good idea to perform
+some other action (type on the keyboard, move the mouse, utilize the
+disks) during the prime generation; this gives the random number
+generator a better chance to gain enough entropy.
+We need to generate a lot of random bytes. It is a good idea to perform
+some other action (type on the keyboard, move the mouse, utilize the
+disks) during the prime generation; this gives the random number
+generator a better chance to gain enough entropy.
+gpg: /home/codespace/.gnupg/trustdb.gpg: trustdb created
+gpg: directory '/home/codespace/.gnupg/openpgp-revocs.d' created
+gpg: revocation certificate stored as '/home/codespace/.gnupg/openpgp-revocs.d/E135CD456C72232ACA436A2119944ACFDF91C658.rev'
+public and secret key created and signed.
+
+pub   rsa4096 2026-03-20 [SC] [expires: 2027-03-20]
+      E135CD456C72232ACA436A2119944ACFDF91C658
+uid                      DIANA CAMILA VILLAGRAN PARRALES (mi primera llave) <dianacamilux@gmail.com>
+sub   rsa4096 2026-03-20 [E] [expires: 2027-03-20]
+
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (main) $ gpg --list-keys
+gpg: checking the trustdb
+gpg: marginals needed: 3  completes needed: 1  trust model: pgp
+gpg: depth: 0  valid:   1  signed:   0  trust: 0-, 0q, 0n, 0m, 0f, 1u
+gpg: next trustdb check due at 2027-03-20
+/home/codespace/.gnupg/pubring.kbx
+----------------------------------
+pub   rsa4096 2026-03-20 [SC] [expires: 2027-03-20]
+      E135CD456C72232ACA436A2119944ACFDF91C658
+uid           [ultimate] DIANA CAMILA VILLAGRAN PARRALES (mi primera llave) <dianacamilux@gmail.com>
+sub   rsa4096 2026-03-20 [E] [expires: 2027-03-20]
+
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (main) $ gpg  -armor --export
+-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+mQINBGm9awYBEADLGnRrIlFKqJfZ/fjCO0ngIkJ2n/sQ0y4T9QFgWDi0u0r65sW+
+bQkZqBNJ3IpsAhw8nsAeFikQJXfK49SXtfU1atOLqFTR2BYy6nIBmlS0ql2hRFDP
+BQysd4mbRNAGSNPgTJx0R/ZdhtrcR7lKblfOr8qkUcaTw9B3k34kVgshMaY4wrJJ
+VR06j6bL/PpNvg3kZ5e8mrKKCycrwvtJDHXgxtX8I5BaBMXI9EFUo0iCWXI23Zf8
+NDqbzVdY+OrqFyEWx0qMU/cOVuuPVsQBgsTAkLhz09ctDTgiosP5bw4yu8rZXFS7
+Dy/7EfdJ1jXQnC6dOaMO44ly7CeC/F0Nyua5gAx3kGxIqZzZYq/ZA59XE/iIqdco
+BiZCvV3EELrg/gpuO8O6uBGcQZpRMIcvi/a0YPxFD38M5AHulceQClkNXNcq5xlz
+FGQnzBBWlZvZuKnvFkhiPySjx9r/YSbou9gtKM+YIPYyHUhRSfKUNmwRHayU7mhP
+iZ0ivRYAbcY9taXeb6aFgVUYAa1r2axnKxoejBM5P/WYRmfcTV4wsWpBRU57QTd8
+gFkA8qzc/l4ofU8ZUL6H6pjNBJ831/3ref+StIqFcP5unkCx0k+KUePIGbeVkMyK
+mY6/F+jaXlrOugzvOAu8RbRjP4YCOu9Bjf3qQZ5nMDM/IIjbxx2lZcyqPQARAQAB
+tEtESUFOQSBDQU1JTEEgVklMTEFHUkFOIFBBUlJBTEVTIChtaSBwcmltZXJhIGxs
+YXZlKSA8ZGlhbmFjYW1pbHV4QGdtYWlsLmNvbT6JAlcEEwEKAEEWIQThNc1FbHIj
+KspDaiEZlErP35HGWAUCab1rBgIbAwUJAeEzgAULCQgHAgIiAgYVCgkICwIEFgID
+AQIeBwIXgAAKCRAZlErP35HGWFiVD/9TDYg+Av2HQt4vjXVtei+3bI0kxTgz5w9N
+b1nUTps5oklTOXkWy2rr+G5CFG5B4saOH7SHyS+RulCvHU2FyygMXPFQso1qX20i
+tigX7e8I++T3Xo6vCPR+HNJM2/DyjTKvRcJNfijS6KHp5PDouvmNiI5kW9dj9BRJ
+Wtyau4lEGTkk7lw1v3UfCbiFv3/r+AqeHqqHQeDWHzdvX0tGL3H6roQZNyU+aX/6
+zTQEK8Z3T2io5gDX3eNvaWnkWjbZkWG5RPbXnch9UUPft3V68fzdakXK7DspJky+
+q6aD5T731spRspYDI7MgfUhIJ2/YPvG6fz8BL+M8OFQNQaRvafpHnxG3ZyNMkwno
+Vch/3cRM8zeB+pCcXHRYCyiBZcDHcqNYc5EPja9tYGihTAe3UpRA/zjZwyD2jVZM
+Ev6kv8G6l57isY+k6fTug+b4uZbnQulF+1oj/gj0OTzjBPI/nwvk1D3DSm2+ovjF
+CPmmOZijaPC9Myvey7ZNC3RY0JRkbaG02XRGzHCt+UnZbMKG+qulTAkcV25Xxw2A
+6QdtbMWHV0eKsAyO/JXFKaZQfnggsXmDKV/ZP6aSiFpbP1EBERkGR1/YVCIo/vcv
+zI+dkGWkBfPErTQMg0ww6E5bbcyE2CaiyoNdnyRJV4T9Hl5Bo2BfBpy8M0IFpCmo
+iG8ybARzCbkCDQRpvWsGARAA2/dbTTugv1lUadFjS0vSuf+xyHZ7SR00/dd+kz17
+QRjBpK2tlcTv0GviJ2qqIRqBx1GT+ZIrTg5VCjC9GtKzXpLuHrp/MMd6Zyj5mie6
+gVwCnXB7k9lcXsw2INS1FFOPTsrQK9KCnj4dpqHL4tfbC+FaPQAf3Va0SieIQ+HF
++4gz0dRQ3Aq99fUd4uisYuPAsV2OB8D57bTPYfvnffBiRWU9hocKdtOf8g+yaJgK
+qErkFVJhxWzV5HyHzcgA9EqlSvyFv5Us7up3iPQeRRYOoKdlRc0lVGWTsGZ+fn4g
+kokpQYZOYtxsllAzPotaBItVPd+uqhq52Xb+r5rNi3buKj7Tgf3b1PlEGSIxKr5K
+RLDwyAcWtVRSQDejPNymDvlUtxrpLdy7+h+cm0pu1gN7rLeHWdQvIvuLP3fEAWMq
+wtxdNoCzUvSCC9C7ei5tlBmnnGzgv30H4C1YII8k32OEG5IR9WBaoMw/M0JrrLaC
+E5RjAKgTQYTkZ9heSqE1Zv/Ase2I6JF+hq2DDRfOWT59185wox4UIPzF2ytaYG1j
+s54C109g96Rb5kUVlndl/lN20YQQomF3R4B/6MsxKAg9xKyFnb7JCbOtvhjWuDEZ
+mp7GVpo5lWVO6EhL+BusqjTV47VgJAMgJVN92qVgrjS+M4Hp91nKT5qY3o+GxBMd
+hScAEQEAAYkCPAQYAQoAJhYhBOE1zUVsciMqykNqIRmUSs/fkcZYBQJpvWsGAhsM
+BQkB4TOAAAoJEBmUSs/fkcZYsQEQAI6LMkKOivtVn3NA0fSpEdXwhF5VoFLFHJxU
+tY1IdZ3f6/FCuCtUnV4GdzQeiPuIkpIfsJeGWuXO7KjlusUaxizdTk5rP/H8HTDX
+MceW69JV9wHe4eT9kQiPXhNBV6zlyOzyDbZJOUvq5D2botjt8ao8oGyQEACaVH94
+vuL7VM6jve9PIr+D557f9ofqRWearZIzteoAyWoDHqsRBS/FIVQr5Q1KCorXX2oz
+VjS3kkprmQY3NUW7QnXZ8gDVzdtozTcQgPYsRmnJzBD0LrMHZ5J7R/5WNLbLDh3M
+jYrUYdrH/MkSecZdGRpwpWkOYH/rqnxiotGRoINQkcjFkkx/Rq2qiHS7KxLoza1O
+KOviYTo9uRg28fyxnW4+LYxWzJyytSITj4zB+ieinLrFbifxvRpwbVBz9as/zsUJ
+8fMJvPkmptXQ2jLCla4hy6mEFSX50xVsEBjlXrgY9B013LqhwavM/4CJP3uXvkKw
+eNJ9jdfAUzp4gl9ln9vZZfFtp5lwzsPU0MIdSkplBloujPcYFDS6JoxQLUiKhAeE
+cFht4Gg698P82nWNPU4hup8PCMyboUVEwIgJDvP7Jh8lXpM4uvN257jGLXv6CF+d
+nV64z/R68mSADistuCIaC32WoWcCoYcQyuSUq7IDLYgF3IYjrymBO22iocqu7N/e
+v5BBpTix
+=kFRF
+-----END PGP PUBLIC KEY BLOCK-----
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (main) $ gpg --armor  --edianac
+amilux7
+gpg: invalid option "--edianacamilux7"
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (main) $ gpg --armor --dianacamilux@gmail.com > mi llave_publica.asc 
+gpg: invalid option "--dianacamilux@gmail.com"
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (main) $ gpd --armor  --export dianacamilux@gmail.com > mi llave_publica.asc
+bash: gpd: command not found
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (main) $ gpg --armor  --export 
+dianacamilux@gmail.com > mi llave_publica.asc
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (main) $ ls
+LICENSE  README.md  calculadora  gnup_script.sh  gnupg_  mi
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (main) $ cat
