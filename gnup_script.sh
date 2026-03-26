9eeed796f8df5d662ccbcfb443ace116b7e01efe
Dicami <divillagranpa@uide.edu.ec>
2026-03-26 18:39:17 +0000

    vote of trust to a key

--- gnup_script.sh
+++ gnup_script.sh
@@ -236,43 +236,42 @@ gpg: Signature made Mon Mar 23 15:44:09 2026 UTC
 gpg:                using RSA key 28140176FF2A98AD077AE20F1F0C857FC076602F
 gpg: Good signature from "DANNA ALEJANDRA SIMALUISA QUIZHPI (mi primera llave en la terminal) <dannaalejandra008@gmail.com>" [full]
 
-
-
-
-$ gpg --edit-key 28140176FF2A98AD077AE20F1F0C857FC076602F
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (main) $ gpg --edit-key 28140176FF2A98AD077AE20F1F0C857FC076602F # A command, we can give a vote of trust to a key
 gpg (GnuPG) 2.4.4; Copyright (C) 2024 g10 Code GmbH
 This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.
-
-
 pub  rsa4096/1F0C857FC076602F
      created: 2026-03-20  expires: 2027-03-20  usage: SC  
-     trust: unknown       validity: unknown
+     trust: full          validity: full
 sub  rsa4096/AC240ACED367542D
      created: 2026-03-20  expires: 2027-03-20  usage: E   
-[ unknown] (1). DANNA ALEJANDRA SIMALUISA QUIZHPI (mi primera llave en la terminal) <dannaalejandra008@gmail.com>
-
+[  full  ] (1). DANNA ALEJANDRA SIMALUISA QUIZHPI (mi primera llave en la terminal) <dannaalejandra008@gmail.com>
 gpg> trust
 pub  rsa4096/1F0C857FC076602F
      created: 2026-03-20  expires: 2027-03-20  usage: SC  
-     trust: unknown       validity: unknown
+     trust: full          validity: full
 sub  rsa4096/AC240ACED367542D
      created: 2026-03-20  expires: 2027-03-20  usage: E   
-[ unknown] (1). DANNA ALEJANDRA SIMALUISA QUIZHPI (mi primera llave en la terminal) <dannaalejandra008@gmail.com>
+[  full  ] (1). DANNA ALEJANDRA SIMALUISA QUIZHPI (mi primera llave en la terminal) <dannaalejandra008@gmail.com>
 
 Please decide how far you trust this user to correctly verify other users' keys
 (by looking at passports, checking fingerprints from different sources, etc.)
 
   1 = I don't know or won't say
-   2 = I do NOT trust
+  2 = I do NOT trust
   3 = I trust marginally
   4 = I trust fully
   5 = I trust ultimately
   m = back to the main menu
 
 Your decision? 4
-$ gpg --sign-key dannaalejandra008@gmail.com
-
+pub  rsa4096/1F0C857FC076602F
+     created: 2026-03-20  expires: 2027-03-20  usage: SC  
+     trust: full          validity: full
+sub  rsa4096/AC240ACED367542D
+     created: 2026-03-20  expires: 2027-03-20  usage: E   
+[  full  ] (1). DANNA ALEJANDRA SIMALUISA QUIZHPI (mi primera llave en la terminal) <dannaalejandra008@gmail.com>
+#no
 gpg: checking the trustdb
 gpg: marginals needed: 3  completes needed: 1  trust model: pgp
 gpg: depth: 0  valid:   1  signed:   0  trust: 0-, 0q, 0n, 0m, 0f, 1u
