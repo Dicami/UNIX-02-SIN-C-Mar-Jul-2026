34b310de3d67f9ac40424d7a9b20711544f24fc3
Dicami <divillagranpa@uide.edu.ec>
2026-03-27 14:45:25 +0000

    wip: proc directory exploration

--- filesystem_script.sh
+++ filesystem_script.sh
@@ -11,4 +11,67 @@ dri              loop-control  mem     pts           sdc    stdout    tty17  tty
 ecryptfs         loop0         mqueue  random        sdc1   tty       tty18  tty28  tty38  tty48  tty58  ttyS1   ttyS2   ttyS3   uhid       vcs6         vcsu3
 fb0              loop1         net     rfkill        sg0    tty0      tty19  tty29  tty39  tty49  tty59  ttyS10  ttyS20  ttyS30  uinput     vcsa         vcsu4
 fd               loop2         null    root          sg1    tty1      tty2   tty3   tty4   tty5   tty6   ttyS11  ttyS21  ttyS31  urandom    vcsa1        vcsu5
-@Dicami ➜ /dev $ 
+@Dicami ➜ /dev $ cd /proc #change directory to proc directory
+@Dicami ➜ /proc $ ls #list process on proc directory
+1      286   8675  9847        cmdline    dma            fs          kcore        kpageflags     misc          partitions  softirqs       thread-self        vmallocinfo
+10314  353   8683  acpi        consoles   driver         interrupts  key-users    latency_stats  modules       pressure    stat           timer_list         vmstat
+10353  512   8813  bootconfig  cpuinfo    dynamic_debug  iomem       keys         loadavg        mounts        schedstat   swaps          tty                zoneinfo
+10704  7     8829  buddyinfo   crypto     execdomains    ioports     kmsg         locks          mtrr          scsi        sys            uptime
+16885  8082  9390  bus         devices    fb             irq         kpagecgroup  mdstat         net           self        sysrq-trigger  version
+16887  8110  9840  cgroups     diskstats  filesystems    kallsyms    kpagecount   meminfo        pagetypeinfo  slabinfo    sysvipc        version_signature
+@Dicami ➜ /proc $ cat cpuinfo #show cpu information
+processor       : 0
+vendor_id       : AuthenticAMD
+cpu family      : 25
+model           : 1
+model name      : AMD EPYC 7763 64-Core Processor
+stepping        : 1
+microcode       : 0xffffffff
+cpu MHz         : 3239.709
+cache size      : 512 KB
+physical id     : 0
+siblings        : 2
+core id         : 0
+cpu cores       : 1
+apicid          : 0
+initial apicid  : 0
+fpu             : yes
+fpu_exception   : yes
+cpuid level     : 13
+wp              : yes
+flags           : fpu vme de pse tsc msr pae mce cx8 apic sep mtrr pge mca cmov pat pse36 clflush mmx fxsr sse sse2 ht syscall nx mmxext fxsr_opt pdpe1gb rdtscp lm constant_tsc rep_good nopl tsc_reliable nonstop_tsc cpuid extd_apicid aperfmperf tsc_known_freq pni pclmulqdq ssse3 fma cx16 pcid sse4_1 sse4_2 movbe popcnt aes xsave avx f16c rdrand hypervisor lahf_lm cmp_legacy svm cr8_legacy abm sse4a misalignsse 3dnowprefetch osvw topoext vmmcall fsgsbase bmi1 avx2 smep bmi2 erms invpcid rdseed adx smap clflushopt clwb sha_ni xsaveopt xsavec xgetbv1 xsaves user_shstk clzero xsaveerptr rdpru arat npt nrip_save tsc_scale vmcb_clean flushbyasid decodeassists pausefilter pfthreshold v_vmsave_vmload umip vaes vpclmulqdq rdpid fsrm
+bugs            : sysret_ss_attrs null_seg spectre_v1 spectre_v2 spec_store_bypass srso
+bogomips        : 4890.85
+TLB size        : 2560 4K pages
+clflush size    : 64
+cache_alignment : 64
+address sizes   : 48 bits physical, 48 bits virtual
+power management:
+
+processor       : 1
+vendor_id       : AuthenticAMD
+cpu family      : 25
+model           : 1
+model name      : AMD EPYC 7763 64-Core Processor
+stepping        : 1
+microcode       : 0xffffffff
+cpu MHz         : 3240.157
+cache size      : 512 KB
+physical id     : 0
+siblings        : 2
+core id         : 0
+cpu cores       : 1
+apicid          : 1
+initial apicid  : 1
+fpu             : yes
+fpu_exception   : yes
+cpuid level     : 13
+wp              : yes
+flags           : fpu vme de pse tsc msr pae mce cx8 apic sep mtrr pge mca cmov pat pse36 clflush mmx fxsr sse sse2 ht syscall nx mmxext fxsr_opt pdpe1gb rdtscp lm constant_tsc rep_good nopl tsc_reliable nonstop_tsc cpuid extd_apicid aperfmperf tsc_known_freq pni pclmulqdq ssse3 fma cx16 pcid sse4_1 sse4_2 movbe popcnt aes xsave avx f16c rdrand hypervisor lahf_lm cmp_legacy svm cr8_legacy abm sse4a misalignsse 3dnowprefetch osvw topoext vmmcall fsgsbase bmi1 avx2 smep bmi2 erms invpcid rdseed adx smap clflushopt clwb sha_ni xsaveopt xsavec xgetbv1 xsaves user_shstk clzero xsaveerptr rdpru arat npt nrip_save tsc_scale vmcb_clean flushbyasid decodeassists pausefilter pfthreshold v_vmsave_vmload umip vaes vpclmulqdq rdpid fsrm
+bugs            : sysret_ss_attrs null_seg spectre_v1 spectre_v2 spec_store_bypass srso
+bogomips        : 4890.85
+TLB size        : 2560 4K pages
+clflush size    : 64
+cache_alignment : 64
+address sizes   : 48 bits physical, 48 bits virtual
+power management:
