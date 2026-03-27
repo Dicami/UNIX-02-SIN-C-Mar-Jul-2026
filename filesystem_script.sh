a4803f53dc6954533f7ea0792497af12e1e7fa32
Dicami <divillagranpa@uide.edu.ec>
2026-03-27 14:39:05 +0000

    wip: devices as files

---
+++ filesystem_script.sh
@@ -0,0 +1,14 @@
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (filesystem) $ cd /dev #We change directory to dv directory   
+@Dicami ➜ /dev $ ls #With that command we can lis our directories 
+autofs           full          loop3   nvme-fabrics  rtc0   sg2       tty10  tty20  tty30  tty40  tty50  tty60   ttyS12  ttyS22  ttyS4      userfaultfd  vcsa2  vcsu6
+bsg              fuse          loop4   nvram         sda    sg3       tty11  tty21  tty31  tty41  tty51  tty61   ttyS13  ttyS23  ttyS5      userio       vcsa3  vfio
+btrfs-control    hpet          loop5   port          sda1   shm       tty12  tty22  tty32  tty42  tty52  tty62   ttyS14  ttyS24  ttyS6      vcs          vcsa4  vga_arbiter
+core             hwrng         loop6   ppp           sda14  snapshot  tty13  tty23  tty33  tty43  tty53  tty63   ttyS15  ttyS25  ttyS7      vcs1         vcsa5  vhost-net
+cpu_dma_latency  input         loop7   psaux         sda15  sr0       tty14  tty24  tty34  tty44  tty54  tty7    ttyS16  ttyS26  ttyS8      vcs2         vcsa6  vhost-vsock
+cuse             kmsg          mapper  ptmx          sdb    stderr    tty15  tty25  tty35  tty45  tty55  tty8    ttyS17  ttyS27  ttyS9      vcs3         vcsu   vmbus
+dma_heap         kvm           mcelog  ptp0          sdb1   stdin     tty16  tty26  tty36  tty46  tty56  tty9    ttyS18  ttyS28  ttyprintk  vcs4         vcsu1  zero
+dri              loop-control  mem     pts           sdc    stdout    tty17  tty27  tty37  tty47  tty57  ttyS0   ttyS19  ttyS29  udmabuf    vcs5         vcsu2  zfs
+ecryptfs         loop0         mqueue  random        sdc1   tty       tty18  tty28  tty38  tty48  tty58  ttyS1   ttyS2   ttyS3   uhid       vcs6         vcsu3
+fb0              loop1         net     rfkill        sg0    tty0      tty19  tty29  tty39  tty49  tty59  ttyS10  ttyS20  ttyS30  uinput     vcsa         vcsu4
+fd               loop2         null    root          sg1    tty1      tty2   tty3   tty4   tty5   tty6   ttyS11  ttyS21  ttyS31  urandom    vcsa1        vcsu5
+@Dicami ➜ /dev $ 
