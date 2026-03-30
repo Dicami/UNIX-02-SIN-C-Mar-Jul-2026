3948cc329db28ed7e2e9191847c52c37f6fef4ae
Dicami <divillagranpa@uide.edu.ec>
2026-03-30 15:50:36 +0000

    stat  command

--- parted_script.sh
+++ parted_script.sh
@@ -11,3 +11,14 @@ Disk Flags:
 @Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (FS_exploration) $ lsblk -f #With that command we can se the second part of our partition table 
 @Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (FS_exploration) $ [ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS" #With that command #With that command is for verify if the file exist 
 BIOS 
+$ echo "mi archivo"
+ > test.txt #with that command we create an archive txt
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (FS_exploration) $ stat test.txt
+  File: test.txt
+  Size: 11              Blocks: 8          IO Block: 4096   regular file
+Device: 7,4     Inode: 1572909     Links: 1
+Access: (0666/-rw-rw-rw-)  Uid: ( 1000/codespace)   Gid: ( 1000/codespace)
+Access: 2026-03-30 15:48:39.804172192 +0000
+Modify: 2026-03-30 15:48:56.234171368 +0000
+Change: 2026-03-30 15:48:56.234171368 +0000
+ Birth: 2026-03-30 15:48:39.804172192 +0000
