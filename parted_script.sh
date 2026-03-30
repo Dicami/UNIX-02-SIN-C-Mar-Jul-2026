293b76f89035dc970420ab142de143713f07c2a3
Dicami <divillagranpa@uide.edu.ec>
2026-03-30 15:09:44 +0000

    install parted

---
+++ parted_script.sh
@@ -0,0 +1,3 @@
+sudo apt update #With that command we update out libreries 
+sudo apt upgrade #With that command we can install the libreries    
+sudo apt install parted #With that command we install the parted
