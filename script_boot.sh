471fee8a79e0e8d75e35c8bcee90b645b9ad6a72
Dicami <divillagranpa@uide.edu.ec>
2026-04-08 21:05:39 +0000

    wip:route types

---
+++ script_boot.sh
@@ -0,0 +1,30 @@
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot) $ ls
+LICENSE
+README.md
+calculadora
+doc_cifrado.txt
+doc_cifrado_y_firmado.txt
+doc_cifrado_y_firrmado_descifrado_y_validado.txt
+doc_no_cifrado.txt
+doc_no_cifrado_firmado.txt
+doc_no_cifrado_firmado_binario.txt
+esposa_doc_cifrado.txt
+esposa_doc_cifrado_y_firmadoo.txt
+esposa_doc_no_cifrado.txt
+esposa_doc_no_cifrado_firmado.txt
+esposa_doc_no_cifrado_firmado_binario.txt
+esposa_firma_separada_doc_no_cifrado.sig
+firma_separada_doc_no_cifrado.sig
+gnup_script.sh
+gnupg_
+llave_esposa_publica.asc
+llave_publica.asc
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot) $ cd calculadora/ #ruta relativa
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora (boot) $ pwd
+/workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora (boot) $ cd /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora #ruta absoluta
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora (boot) $ cd..
+bash: cd..: command not found
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora (boot) $ cd ..
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot) $ touch script_boot.sh
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot) $ 
