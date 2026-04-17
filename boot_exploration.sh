a22143a0a1a061414f84d051875110baa56c4e17
Dicami <divillagranpa@uide.edu.ec>
2026-04-17 14:22:20 +0000

    ls large opcion command

---
+++ boot_exploration.sh
@@ -0,0 +1,12 @@
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls -a #this is thr short opcion
+.           README.md                  doc_cifrado_y_firrmado_descifrado_y_validado.txt  esposa_doc_cifrado_y_firmadoo.txt          firma_separada_doc_no_cifrado.sig
+..          boot_exploration.sh        doc_no_cifrado.txt                                esposa_doc_no_cifrado.txt                  gnup_script.sh
+.git        calculadora                doc_no_cifrado_firmado.txt                        esposa_doc_no_cifrado_firmado.txt          gnupg_
+.gitignore  doc_cifrado.txt            doc_no_cifrado_firmado_binario.txt                esposa_doc_no_cifrado_firmado_binario.txt  llave_esposa_publica.asc
+LICENSE     doc_cifrado_y_firmado.txt  esposa_doc_cifrado.txt                            esposa_firma_separada_doc_no_cifrado.sig   llave_publica.asc
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls --all #this is the large opcion 
+.           README.md                  doc_cifrado_y_firrmado_descifrado_y_validado.txt  esposa_doc_cifrado_y_firmadoo.txt          firma_separada_doc_no_cifrado.sig
+..          boot_exploration.sh        doc_no_cifrado.txt                                esposa_doc_no_cifrado.txt                  gnup_script.sh
+.git        calculadora                doc_no_cifrado_firmado.txt                        esposa_doc_no_cifrado_firmado.txt          gnupg_
+.gitignore  doc_cifrado.txt            doc_no_cifrado_firmado_binario.txt                esposa_doc_no_cifrado_firmado_binario.txt  llave_esposa_publica.asc
+LICENSE     doc_cifrado_y_firmado.txt  esposa_doc_cifrado.txt           
