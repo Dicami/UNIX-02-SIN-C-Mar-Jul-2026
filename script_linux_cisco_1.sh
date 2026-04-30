c6f521a928b94aa90e7bde2efcd7cde0958e12a7
Dicami <divillagranpa@uide.edu.ec>
2026-04-30 21:49:01 +0000

    ls command

---
+++ script_linux_cisco_1.sh
@@ -0,0 +1,21 @@
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (linux_cisco_1) $ ls #With that command we can see our archives in a  list
+LICENSE #the ls command displays a list of information about files.
+README.md
+calculadora
+doc_cifrado.txt
+doc_cifrado_y_firmado.txt
+doc_cifrado_y_firrmado_descifrado_y_validado.txt
+doc_no_cifrado.txt
+doc_no_cifrado_firmado.txt
+doc_no_cifrado_firmado_binario.txt
+esposa_doc_cifrado.txt
+esposa_doc_cifrado_y_firmadoo.txt
+esposa_doc_no_cifrado.txt
+esposa_doc_no_cifrado_firmado.txt
+esposa_doc_no_cifrado_firmado_binario.txt
+esposa_firma_separada_doc_no_cifrado.sig
+firma_separada_doc_no_cifrado.sig
+gnup_script.sh
+gnupg_
+llave_esposa_publica.asc
+llave_publica.asc
