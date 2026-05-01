ba6fed5aedaa5becd652c2741b706925f00282d3
Dicami <divillagranpa@uide.edu.ec>
2026-05-01 16:17:06 +0000

    copy files

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -302,3 +302,7 @@ H is for Happy
 I is for Ink
 J is for Juice
 EOF
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (linux_cisco_1) $ cd camb #We change directory
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/camb (linux_cisco_1) $ cp ../calculadora/cami.sh . #As the course says, we copy cami.sh to another directory
+@Dicami ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/camb (linux_cisco_1) $ ls #With ls we check that directory was copy 
+cami.sh
