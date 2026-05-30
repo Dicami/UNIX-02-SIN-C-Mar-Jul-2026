769ac26f3cdcc583e61a45b0d5b627486f31571a
Dicami <dicami@noreply.codeberg.org>
2026-05-30 21:43:23 +0200

    Using cd and dd comand

---
+++ script_linux_cisco_2.sh
@@ -0,0 +1,5 @@
+sysadmin@localhost:~/Documents$ cd ~ #We changed to directory home                                        
+sysadmin@localhost:~$ dd if=/dev/zero of=/tmp/swapex bs=1M count=50  #dd creates a file using specific arguments and outputs the process statistics        
+50+0 records in                                                                 
+50+0 records out                                                                
+52428800 bytes (52 MB) copied, 0.0274415 s, 1.9 GB/s   
