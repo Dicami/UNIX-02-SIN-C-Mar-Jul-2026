1822f5f0d08164378e7851cf888972404bbe32da
Dicami <divillagranpa@uide.edu.ec>
2026-06-01 15:16:02 +0000

    changed the file to directory

---
+++ blackbashsh1.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash or #!/usr/bin/env bash
+[Camila_Villagran-Dicami-UIDE] /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 # bash --version
+[Camila_Villagran-Dicami-UIDE] /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 # env
+[Camila_Villagran-Dicami-UIDE] /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 # echo ${SHELL}
+[Camila_Villagran-Dicami-UIDE] /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 # echo ${RANDOM}
+[Camila_Villagran-Dicami-UIDE] /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 # echo ${UID}
+[Camila_Villagran-Dicami-UIDE] /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 # echo ${OSTYPE}
+[Camila_Villagran-Dicami-UIDE] /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 # ps -e -f
+[Camila_Villagran-Dicami-UIDE] /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 # ps -ef
+[Camila_Villagran-Dicami-UIDE] /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 # df --human-readable
+[Camila_Villagran-Dicami-UIDE] /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 # df -h
+[Camila_Villagran-Dicami-UIDE] /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 # bash -x blackhatbash1.sh
+[Camila_Villagran-Dicami-UIDE] /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 # bash -r blackhatbash1.sh
