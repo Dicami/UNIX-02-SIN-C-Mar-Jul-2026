d7814a53f2587c77fe37f246ff36a0c45b6e5883
Dicami <divillagranpa@uide.edu.ec>
2026-06-01 14:45:21 +0000

    bash's environment

---
+++ .devcontainer/blackhatbash1.sh
@@ -0,0 +1,6 @@
+[Camila_Villagran-Dicami-UIDE] /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 # bash --version
+[Camila_Villagran-Dicami-UIDE] /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 # env
+[Camila_Villagran-Dicami-UIDE] /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 # echo ${SHELL}
+[Camila_Villagran-Dicami-UIDE] /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 # echo ${RANDOM}
+[Camila_Villagran-Dicami-UIDE] /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 # echo ${UID}
+[Camila_Villagran-Dicami-UIDE] /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 # echo ${OSTYPE}
