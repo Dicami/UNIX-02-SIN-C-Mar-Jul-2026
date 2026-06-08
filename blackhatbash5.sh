22afdc86e5351ca5f8515f7da824a0157804a775
Dicami <divillagranpa@uide.edu.ec>
2026-06-08 14:53:01 +0000

    wip: using ls -l command

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -16,3 +16,10 @@ cat stdout_and_stderr.tx
 ls -l / 1> stdout.txt 2> stderr.txt
 cat stdout.txt
 cat stdout.txt
+lzl 2> error.txt
+cat error.txt
+cat < output.txt
+cat << EOF
+Black Hat Bash
+by No Starch Press
+EOF
