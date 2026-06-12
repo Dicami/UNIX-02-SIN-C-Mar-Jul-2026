4232084890a99bfa0aaf7f462c761db4d5fd1108
Dicami <divillagranpa@uide.edu.ec>
2026-06-12 15:14:14 +0000

    wip: An until loop

--- until_loop.sh
+++ until_loop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FILE="output.txt" #Stores the name of the file in a variable named FILE
+FILE="outpit.txt" #Stores the name of the file in a variable named FILE
 touch "${FILE}" #Creates an empty file named "output.txt"
 until [[ -s "${FILE}" ]]; do #Starts a loop that keeps running UNTIL the file has a size greater than 0
  echo "${FILE} is empty..." #Prints a message stating the file is currently empty
