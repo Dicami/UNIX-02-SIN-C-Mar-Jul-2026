82de30730f195309bcf3c9fd19217209979063ff
Dicami <divillagranpa@uide.edu.ec>
2026-06-19 15:40:31 +0000

    wip: filtering with awk

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -17,3 +17,8 @@ echo "hola1,chao1" > test.csv
 echo "hola2,chao2" >> test.csv
 echo "hola3,chao3" >> test.csv
 awk '{print $1,$NF}' test.csv
+head log.txt
+awk 'NR < 10' log.txt
+grep "42.236.10.117" log.txt #Shows the 7th column (the URLs) for every line in the file.
+awk '{print $7}' log.txt #Filters and shows the full lines belonging only to that IP.
+grep "42.236.10.117" log.txt | awk '{print $7}' #Combines both. Shows only the URLs visited by that specific IP.
